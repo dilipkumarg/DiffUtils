65f7c288029d0ba3eff5b86fe83a90ea222e9eba
gdilipkumar <dilipsgm@gmail.com>
2013-02-27 12:04:02 +0530

    libraries fixed

--- src/main/java/com/DiffLibrary.java
+++ src/main/java/com/DiffLibrary.java
@@ -1,4 +1,4 @@
-package com;
+package main.java.com;
 
 import java.util.List;
 import difflib.*;
--- src/main/java/com/JettyServer.java
+++ src/main/java/com/JettyServer.java
@@ -1,4 +1,4 @@
-package com;
+package main.java.com;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
